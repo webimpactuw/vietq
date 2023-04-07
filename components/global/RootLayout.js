d3a4965121c95cccc79c8ea68622ac997c98aef0
Ritesh Kanchi <ritesh.kanchi@gmail.com>
2023-04-06 21:36:45 -0700

    Create RootLayout.js

---
+++ components/global/RootLayout.js
@@ -0,0 +1,43 @@
+import Head from "next/head";
+
+const defaultTitle = "VietQ";
+const defaultDesc = "This is VietQ's website";
+
+export default function RootLayout({
+  title = defaultTitle,
+  desc = defaultDesc,
+  children,
+}) {
+  return (
+    <>
+      <Head>
+        <title>
+          {title != defaultTitle ? title + ` | ${defaultTitle}` : title}
+        </title>
+        <meta
+          name="title"
+          content={title != defaultTitle ? title + ` | ${defaultTitle}` : title}
+        />
+        <meta name="description" content="{desc}" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://team2714.com/" />
+        <meta
+          property="og:title"
+          content={title != defaultTitle ? title + ` | ${defaultTitle}` : title}
+        />
+        <meta property="og:description" content={desc} />
+        <meta name="apple-mobile-web-app-capable" content="no" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content={title != defaultTitle ? title + ` | ${defaultTitle}` : title}
+        />
+        <link rel="icon" href="favicon.ico" />
+        <link rel="apple-touch-icon" href="app-icon.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="app-icon.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="app-icon.png" />
+      </Head>
+      {children}
+    </>
+  );
+}
