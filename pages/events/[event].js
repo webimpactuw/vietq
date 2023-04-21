8473c9004dc50fe3f3abadd5158b3aefa7056d0c
Ritesh Kanchi <ritesh.kanchi@gmail.com>
2023-04-21 14:51:53 -0700

    random commit to redeploy prod

--- pages/events/[event].js
+++ pages/events/[event].js
@@ -1,5 +1,12 @@
+import Container from "@/components/global/Container";
 import RootLayout from "@/components/global/RootLayout";
 
 export default function Event() {
-  return <RootLayout title="Event"></RootLayout>;
+  return (
+    <RootLayout title="Event">
+      <Container>
+        <h1>Event</h1>
+      </Container>
+    </RootLayout>
+  );
 }
