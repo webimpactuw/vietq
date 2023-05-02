93bce28c5acc5f34221c66935d455c0a87563c66
hLa09 <122062156+hLa09@users.noreply.github.com>
2023-05-02 13:08:55 -0700

    whitespace

--- components/global/Navbar.js
+++ components/global/Navbar.js
@@ -1,69 +1,24 @@
 import Link from "next/link";
-import { HorizontalNavigation } from "./Navigation";
-import { useScrollData } from "scroll-data-hook";
+import React from "react";
 
-import vietQWhite from "../../public/logos/vietq_logo_white.png";
-import vietQBlack from "../../public/logos/vietq_logo_black.png";
-
-import Image from "next/image";
-
-export default function Navbar({ transparent = false }) {
-  const {
-    scrolling,
-    time,
-    speed,
-    direction,
-    position,
-    relativeDistance,
-    totalDistance,
-  } = useScrollData({
-    onScrollStart: () => {
-      console.log("Started scrolling");
-    },
-    onScrollEnd: () => {
-      console.log("Finished scrolling");
-    },
-  });
-
-  const past = position.y > 200;
-
+export default function Navbar({}) {
   return (
-    <nav
-      className={`fixed top-0 w-full transition-colors border-b ${
-        transparent
-          ? past
-            ? "text-blue-800 bg-champagne border-blue-900"
-            : "text-white bg-champagne/0 border-transparent"
-          : "text-blue-800 bg-champagne border-blue-900"
-      } z-40 `}
-    >
-      <div className="gradient h-2" />
-      <div className="mx-auto max-w-6xl py-4 px-4 space-y-4">
-        <div className="flex items-center justify-between space-x-16">
-          <Link href="/">
-            <Image
-              src={transparent ? (past ? vietQBlack : vietQWhite) : vietQBlack}
-              alt="VietQ Logo"
-              className="w-16 hover:opacity-75 transition-opacity"
-              priority={true}
-            />
-          </Link>
-          <HorizontalNavigation noHome={true}>
-            <Link
-              href="/donate"
-              className={`${
-                transparent
-                  ? past
-                    ? "text-white bg-blue-800"
-                    : "text-blue-800  bg-white"
-                  : "text-white bg-blue-800"
-              } uppercase tracking-widest text-xs hover:bg-gray-300 rounded-full px-3 py-1.5 transition-colors font-semibold`}
-            >
-              Donate
-            </Link>
-          </HorizontalNavigation>
+    <>
+      <div className="flex justify-between space-x-16 items-center h-16 mx-6">
+        <Link href="" className="">VietQ</Link>
+        <div className="space-x-8 tracking-widest flex items-center justify-end">
+          <NavLink href="">Home</NavLink>
+          <NavLink href="">About</NavLink>
+          <NavLink href="">Contact</NavLink>
+          <NavLink href="">Community</NavLink>
+          <NavLink href="">Contact</NavLink>
+          <Link href="" className="bg-black text-white font-extrabold rounded-full font-sans py-2 px-4 uppercase">Donate</Link>
         </div>
       </div>
-    </nav>
+    </>
   );
-}
+  }
+
+  function NavLink({children, href}) {
+    return  <Link href={href} className="hover:text-slate-400 text-black font-bold font-sans uppercase">{children}</Link>
+  }
